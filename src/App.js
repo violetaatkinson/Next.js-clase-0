b4a662bf9f130232f9f977a89af6da9e7fa1fc12
violeta atkinson <violeta.atkinson@gmail.com>
2023-10-25 19:43:51 +0200

    listComponent done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import FormComponent from './clase-0/FormComponent.jsx'
+import ListComponent from './clase-0/ListComponent.jsx'
 
 function App() {
   return (
     <div className="App">
       <FormComponent/>
+      <ListComponent/>
     </div>
   );
 }
--- src/clase-0/ListComponent.jsx
+++ src/clase-0/ListComponent.jsx
@@ -5,3 +5,53 @@
 // Utiliza el hook useState para administrar el estado de los campos del formulario.
 // Agrega manejadores de eventos para los cambios en los campos de entrada (onChange) y actualiza el estado.
 // Utiliza el valor del estado para mostrar los valores ingresados en tiempo real debajo del formulario.
+
+import React, { useState } from 'react';
+
+ const ListComponent =() => {
+    const [ formUsers, setFormUsers ] = useState({
+        name: '',
+        email: '',
+    });
+
+    const handleOnChange = (e) => { //contiene información sobre el cambio que ocurrira en el elemento del formulario.
+        const { name, value} = e.target // desestructuración que extrae dos propiedades del elemento input.
+
+        setFormUsers({ 
+            ...formUsers,
+            [name]: value,
+          });
+    }
+
+    return (
+        <div>
+            <form>
+                <label>
+                    Name:
+                    <input
+                        name='name'
+                        value={formUsers.name}
+                        onChange={handleOnChange}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                        name='email'
+                        value={formUsers.email}
+                        onChange={handleOnChange}
+                    />
+                </label>
+            </form>
+
+            <div>
+                <h2>Valores ingresados:</h2>
+                <p>Name: {formUsers.name}</p>
+                <p>Email: {formUsers.email}</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default ListComponent;
